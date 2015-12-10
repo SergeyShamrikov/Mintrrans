8b17799e01c0b087614f34a7e9086cc05082878e
sword3d <pavel.buynovskiy@gmail.com>
2015-12-10 16:44:22 +0200

    Правки таблиц

--- js/script.plugins.js
+++ js/script.plugins.js
@@ -8,6 +8,12 @@
 				Name pudin
 		------------------------------------------------ */
 
+			$(".box .block_ins").show();
+            $(".closed .block_ins").hide();
+
+            $(".drop_menu").click(function(){
+                $(this).toggleClass("show_icon").parent("td").parent("tr").toggleClass("open_bg").nextUntil(".open_box").stop().slideToggle("medium");
+            });
 
         /* ------------------------------------------------
 				End of Name pudin
@@ -19,13 +25,13 @@
 
 		/* ------------------------------------------------
 				Table open
-		------------------------------------------------ */
+		------------------------------------------------ 
 			$(".box .block_ins").show();
             $(".closed .block_ins").hide();
 
             $(".drop_menu").click(function(){
                 $(this).toggleClass("show_icon").parent("td").parent("tr").toggleClass("open_bg").parent("tbody").parent("table").parent(".open_box").next().stop().slideToggle("medium");
-            });
+            });*/
 
         /* ------------------------------------------------
 				End Table open
