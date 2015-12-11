5267fae0679cff4c60839df6a15514c237208227
sword3d <pavel.buynovskiy@gmail.com>
2015-12-11 12:17:27 +0200

    Правки

--- js/script.core.js
+++ js/script.core.js
@@ -9,6 +9,7 @@
 			var self = this;
 
 			self.menu();
+			self.table_open();
 		},
 
 		windowLoad: function(){
@@ -30,6 +31,19 @@
 		    	$(this).toggleClass("active");
 
 		    });
+
+		},
+
+
+
+		table_open : function(){
+
+		    $(".box .block_ins").show();
+            $(".closed .block_ins").hide();
+
+            $(".drop_menu").click(function(){
+                $(this).toggleClass("show_icon").closest(".open_box").toggleClass("open_bg").next(".table_box_ins").find(".block_ins").stop().slideToggle("medium");
+            });
 
 		}
 
--- js/script.plugins.js
+++ js/script.plugins.js
@@ -5,18 +5,12 @@
 	$(document).ready(function(){
 
 		/* ------------------------------------------------
-				Name pudin
+				Table open
 		------------------------------------------------ */
 
-			$(".box .block_ins").show();
-            $(".closed .block_ins").hide();
-
-            $(".drop_menu").click(function(){
-                $(this).toggleClass("show_icon").parent("td").parent("tr").toggleClass("open_bg").nextUntil(".open_box").stop().slideToggle("medium");
-            });
-
+		
         /* ------------------------------------------------
-				End of Name pudin
+				End Table open
 		------------------------------------------------ */
 
 	});
@@ -26,12 +20,7 @@
 		/* ------------------------------------------------
 				Table open
 		------------------------------------------------ 
-			$(".box .block_ins").show();
-            $(".closed .block_ins").hide();
-
-            $(".drop_menu").click(function(){
-                $(this).toggleClass("show_icon").parent("td").parent("tr").toggleClass("open_bg").parent("tbody").parent("table").parent(".open_box").next().stop().slideToggle("medium");
-            });*/
+			
 
         /* ------------------------------------------------
 				End Table open
